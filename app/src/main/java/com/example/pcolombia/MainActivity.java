09b7b2e04d0df38f764c617b2784341ea68730dd
Juan Pablo Moya Alarcón <jmoya@unal.edu.co>
2021-08-11 10:20:49 -0500

    Primer Commit

---
+++ app/src/main/java/com/example/pcolombia/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.pcolombia;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        /*getSupportActionBar().setBackgroundDrawable(new ColorDrawable(Color.parseColor("#EE7729")));
+        getSupportActionBar().setTitle("PColombia");*/
+        getSupportActionBar().hide();
+    }
+}
