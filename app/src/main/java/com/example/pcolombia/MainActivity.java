1885d579ac46d269bc309405129c2432b3309a53
Juan Pablo Moya Alarcón <jmoya@unal.edu.co>
2021-08-12 12:21:12 -0500

    Se hace el diseño de Gestionar Usuario, dejando datos de ejemplo que serán removidos proximente

--- app/src/main/java/com/example/pcolombia/MainActivity.java
+++ app/src/main/java/com/example/pcolombia/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.pcolombia;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,5 +15,10 @@ public class MainActivity extends AppCompatActivity {
         /*getSupportActionBar().setBackgroundDrawable(new ColorDrawable(Color.parseColor("#EE7729")));
         getSupportActionBar().setTitle("PColombia");*/
         getSupportActionBar().hide();
+    }
+
+    public void pasarPestena(View view){
+        Intent activity = new Intent(this,GestionarUsuario.class);
+        startActivity(activity);
     }
 }
