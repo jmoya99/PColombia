90e42af07b6b5a9dc63fb58af45c85f1f3c85515
Juan Pablo Moya Alarcón <jmoya@unal.edu.co>
2021-08-21 12:12:06 -0500

    Se agregan los metodos abstractos de las interfaces Room en el LocalStorage

--- app/src/main/java/com/example/pcolombia/model/LocalStorage.java
+++ app/src/main/java/com/example/pcolombia/model/LocalStorage.java
@@ -6,10 +6,15 @@ import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
 
+import com.example.pcolombia.model.dao.ProductRoomDao;
+import com.example.pcolombia.model.dao.UserRoomDao;
 import com.example.pcolombia.model.pojo.*;
 
 @Database(entities = {User.class, Product.class},version = 1)
 public abstract class LocalStorage extends RoomDatabase {
+
+    public abstract UserRoomDao userRoomDao();
+    public abstract ProductRoomDao productRoomDao();
 
     private static LocalStorage localStorage;
 
